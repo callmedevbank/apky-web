e76c1b5426d1e11fc296bfcca3ce092d334acd2a
idrisajala2 <ajalaidris90@gmail.com>
2022-02-13 20:50:13 +0100

    good push idris

---
+++ App.js
@@ -0,0 +1,273 @@
+
+import './App.css';
+import svg1 from "./images/ch1.png"
+import svg2 from "./images/1.png"
+import svg3 from "./images/1.png"
+import svg4 from "./images/feature11.png"
+import svg5 from "./images/1.png"
+import svg6 from "./images/1.png"
+import svg7 from "./images/1.png"
+import svg8 from "./images/1.png"
+import svg9 from "./images/play.jpg"
+import svg10 from "./images/feature11.png"
+import svg11 from "./images/1.png"
+
+function App() {
+  return (
+     <div>
+       <div class="header">
+         <div class="head">
+         <div class="logo">
+           <div>Apkly.</div>
+         </div>
+         <div class="nav">
+           <span>Home</span>
+           <span>Overview</span>
+           <span>Feature</span>
+           <span>Price</span>
+           <span>Contact</span>
+           <button>Sign Up</button>
+       </div>
+     </div>
+   </div>
+         <div class="hero">
+           <div class ="hero1">
+           <div class="hero2">
+           <div class="words">
+             <div class="aw">Awesome</div>
+             <div class="ap">App of the year</div>
+             <div class="ip">leorem ipsom dewip leprium
+               gereas uitre .....
+             derouim ipsom dewip leprium
+               gereas uitre 
+             </div>
+                <button>click it now</button>
+           </div>
+          <div class="mand" > 
+       <img alt="pdf" src={svg1}></img>
+            </div>
+        </div>
+        </div>
+     </div>
+     {/* {................................................} */}
+     <div class="over">
+       <div class="over2">
+         <h1>OVERVIEW</h1>
+           <div class="box1">
+           <div class="b1">
+           <img alt="pdf" src={svg2}></img>
+                   <div>
+                     <h4>Best Designs</h4>
+                     <div>CorelDRAW Graphics Suite X7
+Purchase serial number: DR17R31-SRR4YP2-HY4A5A8-STY22VN
+Activation Code:5F45-6AA0-6E39-E4AD-DB4D</div>
+                   </div>
+                   </div>
+         <div class="b1">
+         <img alt="pdf" src={svg3}></img>
+                   <div>
+                     <h4>Easy Setup</h4>
+                     <div>CorelDRAW Graphics Suite X7
+Purchase serial number: DR17R31-SRR4YP2-HY4A5A8-STY22VN
+Activation Code:5F45-6AA0-6E39-E4AD-DB4D</div>
+                   </div>
+           </div>
+         </div>
+       </div>
+
+     </div>
+     {/* {.................................} */}
+     <div class="unlock">   
+        <div class="unlock2">
+          <div class="uk">
+           <img alt="pdf" src={svg4}></img>
+          </div>
+          <div class="best">
+            <div class="bt">UNLOCK THE BEST.</div>
+            <div class="hr">App Feature Here</div>
+              <div class="dr">Purchase serial number: DR17R31-SRR4YP2-HY4A5A8-STY22VN
+Activation Code:5F45-6AA0-6E39-E4AD-DB4</div>
+         <div class="packs">
+                     <div class="dis">
+           <img alt="pdf" src={svg5}></img>
+           <div>Purchase serial number: DR17R31-SRR4YP2 lorem</div>
+                     </div>
+               <div class="dis">
+           <img alt="pdf" src={svg6}></img>
+           <div>Purchase serial number: DR17R31-SRR4YP2</div>
+              </div>
+              <div class="dis">
+           <img alt="pdf" src={svg7}></img>
+           <div>Purchase serial number: DR17R31-SRR4YP2</div>
+             </div>
+                  <div class="dis">
+           <img alt="pdf" src={svg8}></img>
+           <div>Purchase serial number: DR17R31-SRR4YP2</div>
+                   </div>
+
+          </div>
+        </div>
+     </div>
+ </div>
+ {/* {..................................................} */}
+ <div class="play">
+   <div class="play2">
+     <div class="pt">
+     <div class="down">
+       <div class="download">3.56</div>
+       <div>download</div>
+     </div>
+     <p class="down">
+       <div class="download1">500M</div>
+       <div>Active User</div>
+     </p>
+     </div>
+
+     <div class="update">
+     <div class="down">
+       <div class="download2">3.56</div>
+       <div>Rating</div>
+     </div>
+     <p class="down">
+       <div class="download3">500M</div>
+       <div>update</div>
+     </p>
+     </div>
+  <div class="piv">
+    <div class="piv2">
+      <img alt="pdf" src={svg9}></img>
+    </div>
+    <div>watch our story</div>
+           </div>
+      </div>
+    </div>
+    {/* {..............................................} */}
+    <div class="basic">
+      <div class="basic1">
+    <div class="limit">
+      <div class="limit1">
+        <div class="bs">Basic</div>
+        <div class="mt">$10/month</div>
+        <p>25 gb backup</p>
+        <p>limited support</p>
+        <p>Fastest load speed</p>
+        <div class="bn">Buy Now</div>
+      </div>
+    </div>
+    <div class="limit">
+      <div class="limit1">
+        <div class="bs">Free</div>
+        <div class="mt">$0/month</div>
+        <p>25 gb backup</p>
+        <p>limited support</p>
+        <p>Fastest load speed</p>
+        <div class="bk">Download</div>
+      </div>
+    </div>
+    <div class="limit">
+      <div class="limit1">
+        <div class="bs">Advance</div>
+        <div class="mt">$35/month</div>
+        <p>25 gb backup</p>
+        <p>limited support</p>
+        <p>Fastest load speed</p>
+        <div class="bn">Buy Now</div>
+      </div>
+    </div>
+         </div>
+          </div>
+          {/* {.....................................} */}
+
+          <div class="hero">
+           <div class ="hero1">
+           <div class="hero2">
+           <div class="words">
+             <div class="rem">NEED MORE INFORMATION?</div>
+             <div class="api">Ask Our community</div>
+             <p>01.App User Experience & interface?</p>
+             <div class="ip">leorem ipsom dewip leprium
+               gereas uitre .....
+             derouim ipsom dewip leprium
+               gereas uitre 
+             </div>
+             <p>02. App performance and Security</p>
+             <p>03. Best Color Visitors & combination</p>
+                <button>click it now</button>
+           </div>
+          <div class="need" > 
+       <img alt="pdf" src={svg10}></img>
+            </div>
+        </div>
+        </div>
+     </div>
+     {/* {...........................................}        */}
+            
+<div class="point1">
+<div class="do">DOWNLOAD</div>
+  <div class="point2">
+    <div class="ads">
+    <div class="van">App advantages</div>
+     <div class="van2">DOWNLOAD</div>
+     <p >Purchase serial number: DR17R31-SRR4YP2-HY4A5A8-STY22VN
+Activation Code:5F45-6AA0-6E39-E4AD-DB4</p>
+<p>leorem ipsom dewip leprium
+               gereas uitre .....
+             derouim ipsom dewip leprium
+               gereas uitre </p>
+               < p class="pen">Open Now</p>
+     </div>
+     <div class="apple">
+       <div class="apple1">
+         <div class="is1">
+         <img alt="pdf" src={svg11}></img>
+         </div>
+         <h3>apple</h3>
+         <p>  gereas uitre .
+             derouim ipsom dewip leprium
+               gereas uitre</p>
+               <div class="bn1">download</div>
+         </div>
+       <div class="apple2">
+         <div class="is">
+       <img alt="pdf" src={svg11}></img>
+       </div>
+       <h3>andriod</h3>
+       <p>  gereas uitre .
+             derouim ipsom dewip leprium
+               gereas uitre</p>
+               <div class="bn">download</div>
+
+       </div>
+     </div>
+     <div class="apple">
+       <div class="apple1">
+         <div class="is1">
+         <img alt="pdf" src={svg11}></img>
+         </div>
+         <h3>otimize</h3>
+         <p>  gereas uitre .
+             derouim ipsom dewip leprium
+               gereas uitre</p>
+               <div class="bn1">download</div>
+         </div>
+       <div class="apple2">
+         <div class="is">
+       <img alt="pdf" src={svg11}></img>
+       </div>
+       <h3>store</h3>
+       <p>  gereas uitre .
+             derouim ipsom dewip leprium
+               gereas uitre</p>
+               <div class="bn">download</div>
+
+       </div>
+     </div>
+  </div>
+</div>
+            
+  
+ </div>
+  );
+}
+
+export default App;
